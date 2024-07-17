7ff88be7af299ebd807b4ddef6112b66c57b8d6e
BothimTV <development@bothimtv.com>
2024-07-17 15:02:22 +0200

    Fixed invalid time format bug

--- src/builder/ShortUrlBuilder.ts
+++ src/builder/ShortUrlBuilder.ts
@@ -62,13 +62,32 @@ export class ShortUrlBuilder {
         return this;
     }
 
-    public setValidSince(date: Date): ShortUrlBuilder {
-        this.validSince = date.toISOString();
+    // https://stackoverflow.com/a/17415677
+    private formatTime(date: Date | number): string {
+        date = typeof date == "number" ? new Date(date) : date
+        var tzo = -date.getTimezoneOffset(),
+            dif = tzo >= 0 ? '+' : '-',
+            pad = function(num: number) {
+                return (num < 10 ? '0' : '') + num;
+            };
+
+        return date.getFullYear() +
+            '-' + pad(date.getMonth() + 1) +
+            '-' + pad(date.getDate()) +
+            'T' + pad(date.getHours()) +
+            ':' + pad(date.getMinutes()) +
+            ':' + pad(date.getSeconds()) +
+            dif + pad(Math.floor(Math.abs(tzo) / 60)) +
+            ':' + pad(Math.abs(tzo) % 60);
+    }
+
+    public setValidSince(date: Date | number): ShortUrlBuilder {
+        this.validSince = this.formatTime(date);
         return this;
     }
 
-    public setValidUntil(date: Date): ShortUrlBuilder {
-        this.validUntil = date.toISOString();
+    public setValidUntil(date: Date | number): ShortUrlBuilder {
+        this.validUntil = this.formatTime(date);
         return this;
     }
 
